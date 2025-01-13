1ae7af8b22f1808ee5b0bb8d81dbb8a95d018b5a
loremispum2k8 <141552551+loremispum2k8@users.noreply.github.com>
2025-01-13 21:27:30 +0200

    Finish the search bar

--- script.js
+++ script.js
@@ -3,16 +3,74 @@
 const searchBar = document.getElementById('search-bar-container');
 const searchBarPopUp = document.querySelector('.total-SEARCH-POPUP');
 const cancelBtn = document.getElementById('Cancel')
+const titlesSection = document.querySelector('.search-tracker-row')
 searchBar.addEventListener('click', ()=>{
     searchBarPopUp.classList.add('searchAnimation');
     document.body.style.overflow = 'hidden';
 })
 cancelBtn.addEventListener('click', ()=>{
-    console.log('a')
     searchBarPopUp.classList.remove('searchAnimation');
     document.body.style.overflowY = 'scroll';
 })
 
+
+const inputSearch = document.querySelector('.searchInput')
+const itemNames = document.querySelectorAll('#item-title');
+let itemTitles = [];
+for(let i = 0; i<itemNames.length; i++){
+    itemTitles.push(itemNames[i].textContent.toLowerCase().split(''))
+}
+
+
+
+
+inputSearch.addEventListener('input', (e)=>{
+    titlesSection.innerHTML = ""
+
+    let titlesCopyArr = [];
+    itemTitles.forEach(item => titlesCopyArr.push(item))
+    
+                    let inputValue = [];
+                    inputValue.push((e.target.value.toLowerCase()));
+    
+
+    let usableInputValue = [];
+    usableInputValue = String(inputValue).split('');
+
+    let finalReferenceArrays = []
+    let finalReferenceGroup = []
+    titlesCopyArr.forEach(item => {
+        let x = usableInputValue.length;
+        finalReferenceArrays = item.slice(0, x);
+        finalReferenceGroup.push(finalReferenceArrays)
+    })
+
+    console.log(finalReferenceGroup)
+    console.log(usableInputValue)
+
+    finalReferenceGroup.forEach((item) => {
+        if(item.join('') === usableInputValue.join('')){
+            if(usableInputValue !== '' && usableInputValue.length !== 0){
+                searches = document.createElement('a');
+                searches.textContent = ((titlesCopyArr[finalReferenceGroup.indexOf(item)]).join('')).toUpperCase();
+                searches.href = '#'+searches.textContent;
+                titlesSection.appendChild(searches);
+                searches.addEventListener('click', ()=>{
+                    searchBarPopUp.classList.remove('searchAnimation');
+                    document.body.style.overflowY = 'scroll';  
+                })
+            }
+        }
+    })
+
+})
+
+
+
+
+
+
+//     FADE ANIMATION      //
 
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
